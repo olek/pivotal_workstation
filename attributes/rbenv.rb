b04a792f5b382878c7918775239a3911c5349e64
Olek Poplavsky <olek@woodenbits.com>
2012-09-20 12:23:59 -0400

    lets build those rubies with proper readline

--- attributes/rbenv.rb
+++ attributes/rbenv.rb
@@ -1,9 +1,15 @@
+readline_prefix = `brew --prefix readline`.chomp
+
+readline_option_new = readline_option_old = ''
+if readline_prefix =~ /Cellar/
+  readline_option_new = %Q(CONFIGURE_OPTS="--enable-shared --with-readline-dir=#{readline_prefix}")
+  readline_option_old = %Q(CONFIGURE_OPTS="-c --enable-shared -c --with-readline-dir=#{readline_prefix}")
+end
+
 node.default["rbenv"]= {
   "rubies" => {
-      "ree-1.8.7-2012.01" => {},
-      "1.8.7-p352" => {},
-      "1.9.2-p290" => {},
-      "1.9.3-p125" => { :env => "CC=clang"}
+      "ree-1.8.7-2011.03" => { :env => readline_option_old },
+      "1.9.3-p194" => { :env => readline_option_new }
     },
-  "default_ruby" => "1.9.3-p125"
+  "default_ruby" => "1.9.3-p194"
 }
