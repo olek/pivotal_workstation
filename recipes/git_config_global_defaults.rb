5ddb643ff1118a6e36f057de0628c3c3566ba45b
Olek Poplavsky <olek@woodenbits.com>
2012-09-26 09:28:15 -0400

    refactored git config recipe to be more concise and quieter

--- recipes/git_config_global_defaults.rb
+++ recipes/git_config_global_defaults.rb
@@ -9,78 +9,46 @@ template "#{WS_HOME}/.gitignore_global" do
   )
 end
 
-execute "set global git ignore" do
-  command "git config --global core.excludesfile #{WS_HOME}/.gitignore_global"
-  user WS_USER
-  not_if "git config --global core.excludesfile | grep -q .gitignore_global"
-end
+git_configs = {
+  'core.excludesfile' =>
+    "#{WS_HOME}/.gitignore_global",
+  'alias.st' =>
+    'status',
+  'alias.di' =>
+    'diff',
+  'alias.co' =>
+    'checkout',
+  'alias.ci' =>
+    'commit',
+  'alias.br' =>
+    'branch',
+  'alias.sta' =>
+    'stash',
+  'apply.whitespace' =>
+    'nowarn',
+  'color.branch' =>
+    'auto',
+  'color.diff' =>
+    'auto',
+  'color.interactive' =>
+    'auto',
+  'color.status' =>
+    'auto',
+  'color.ui' =>
+    'auto',
+  'branch.autosetupmerge' =>
+    'true'
+}
 
-execute "set alias st=status" do
-  command "git config --global alias.st status"
-  user WS_USER
-end
-
-execute "set alias di=diff" do
-  command "git config --global alias.di diff"
-  user WS_USER
-end
-
-execute "set alias co=checkout" do
-  command "git config --global alias.co checkout"
-  user WS_USER
-end
-
-execute "set alias ci=commit" do
-  command "git config --global alias.ci commit"
-  user WS_USER
-end
-
-execute "set alias br=branch" do
-  command "git config --global alias.br branch"
-  user WS_USER
-end
-
-execute "set alias sta=stash" do
-  command "git config --global alias.sta stash"
-  user WS_USER
+git_configs.each do |k, v|
+  execute "set global git config #{k}" do
+    command "git config --global #{k} #{v}"
+    user WS_USER
+    not_if "git config --global #{k} | grep -q #{v}"
+  end
 end
 
 execute "set alias llog=log --date=local" do
   command "git config --replace-all --global alias.llog 'log --date=local'"
-  user WS_USER
-end
-
-execute "set apply whitespace=nowarn" do
-  command "git config --global apply.whitespace nowarn"
-  user WS_USER
-end
-
-execute "set color branch=auto" do
-  command "git config --global color.branch auto"
-  user WS_USER
-end
-
-execute "set color diff=auto" do
-  command "git config --global color.diff auto"
-  user WS_USER
-end
-
-execute "set color interactive=auto" do
-  command "git config --global color.interactive auto"
-  user WS_USER
-end
-
-execute "set color status=auto" do
-  command "git config --global color.status auto"
-  user WS_USER
-end
-
-execute "set color ui=auto" do
-  command "git config --global color.ui auto"
-  user WS_USER
-end
-
-execute "set branch autosetupmerge=true" do
-  command "git config --global branch.autosetupmerge true"
   user WS_USER
 end
