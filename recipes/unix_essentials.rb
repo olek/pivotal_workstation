bc854f8c8ee1091662a18fc43ed0eda993cab025
Olek Poplavsky <olek@woodenbits.com>
2012-10-23 16:32:23 -0400

    disabled 'watch' and 'ssh-copy-id' brew packages because they seem to be unavailable anymore

--- recipes/unix_essentials.rb
+++ recipes/unix_essentials.rb
@@ -5,8 +5,8 @@ if node["kernel"]["release"] == "12.0.0"
   include_recipe "pivotal_workstation::xquartz"
 end
 
-brew_install "watch"
-brew_install "ssh-copy-id"
+#brew_install "watch"
+#brew_install "ssh-copy-id"
 brew_install "pstree"
 brew_install "tree"
 brew_install "wget"
