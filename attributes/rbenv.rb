dbc1f1f8a70e6468925aa5000f901f3585a4e58e
Olek Poplavsky <olek@woodenbits.com>
2013-05-14 18:43:14 -0400

    fixed imagemagick/rbenv

--- attributes/rbenv.rb
+++ attributes/rbenv.rb
@@ -1,26 +1,21 @@
-=begin
 readline_prefix = `brew --prefix readline`.chomp
 
-readline_option_new = readline_option_old = ''
-if readline_prefix =~ /Cellar/
-  readline_option_new = %Q(CONFIGURE_OPTS="--enable-shared --with-readline-dir=#{readline_prefix}")
-  readline_option_old = %Q(CONFIGURE_OPTS="-c --enable-shared -c --with-readline-dir=#{readline_prefix}")
+readline_env_new = readline_env_old = {}
+if readline_prefix =~ /opt/
+  readline_env_old = {
+    'CONFIGURE_OPTS' => %Q(-c --enable-shared -c --with-readline-dir=#{readline_prefix})
+  }
+  readline_env_new = {
+    'CONFIGURE_OPTS' => %Q(--enable-shared --with-readline-dir=#{readline_prefix}),
+    'CC' => 'clang'
+  }
 end
-
-node.default["rbenv"]= {
-  "rubies" => {
-      "ree-1.8.7-2011.03" => { :env => readline_option_old },
-      "1.9.3-p194" => { :env => readline_option_new }
-    },
-  "default_ruby" => "1.9.3-p194"
-}
-=end
 
 unless node["rbenv"]
   node.default["rbenv"]= {
     "rubies" => {
-        "ree-1.8.7-2011.03" => {},
-        "1.9.3-p194" => { :env => {"CC" => "clang"}}
+        "ree-1.8.7-2011.03" => { :env => readline_env_old },
+        "1.9.3-p194" => { :env => readline_env_new }
       },
     "default_ruby" => "1.9.3-p194"
   }
--- recipes/imagemagick.rb
+++ recipes/imagemagick.rb
@@ -3,3 +3,16 @@ Chef::Log.warn 'Please use https://github.com/pivotal-sprout/sprout instead'
 brew "imagemagick" do
   options '--disable-openmp'
 end
+
+imagemagick_prefix = `brew --prefix imagemagick`.chomp
+imagemagick_lib_folder = "#{imagemagick_prefix}/lib"
+
+%w(libMagick++ libMagickCore libMagickWand).each do |name|
+  from_path = "#{imagemagick_lib_folder}/#{name}.dylib"
+  to_path = "#{imagemagick_lib_folder}/#{name}-Q16.7.dylib"
+
+  link from_path do
+    to to_path
+    owner WS_USER
+  end
+end
